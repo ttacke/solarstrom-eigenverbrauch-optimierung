e5932a342144b15a1c5b1645a48b786b5eeaa26d
ttacke <ttacke@seibert-media.net>
2023-02-02 19:12:55 +0100

    Webserver reaktiviert; Daten als JSON ausgegeben

--- code/esp32-webserver/esp32-webserver.ino
+++ code/esp32-webserver/esp32-webserver.ino
@@ -9,14 +9,14 @@
 #include "config.h"
 #include "wlan.h"
 #include "web_client.h"
-//#include "web_presenter.h"
+#include "web_presenter.h"
 #include "elektro_anlage.h"
 #include "wechselrichter_leser.h"
 
 Local::Config cfg;
 Local::Wlan wlan(cfg.wlan_ssid, cfg.wlan_pwd);
 Local::WebClient web_client(wlan.client);
-//Local::WebPresenter web_presenter(cfg, wlan);
+Local::WebPresenter web_presenter(cfg, wlan);
 
 void setup(void) {
 	Serial.begin(cfg.log_baud);
@@ -25,20 +25,20 @@ void setup(void) {
 	Serial.println();
 	Serial.println("Setup ESP");
 	wlan.connect();
-//	web_presenter.webserver.add_page("/", []() {
-//		web_presenter.zeige_hauptseite();
-//		Serial.printf("Free stack: %u heap: %u\n", ESP.getFreeContStack(), ESP.getFreeHeap());
-//	});
-//	web_presenter.webserver.start();
+	web_presenter.webserver.add_page("/", []() {
+		web_presenter.zeige_hauptseite();
+		Serial.printf("Free stack: %u heap: %u\n", ESP.getFreeContStack(), ESP.getFreeHeap());
+	});
+	web_presenter.webserver.start();
 	Serial.printf("Free stack: %u heap: %u\n", ESP.getFreeContStack(), ESP.getFreeHeap());
 }
 
 void loop(void) {
-//	web_presenter.webserver.watch_for_client();
-	Local::WechselrichterLeser wechselrichter_leser(cfg, web_client);
-	Local::ElektroAnlage elektroanlage;
-	wechselrichter_leser.daten_holen_und_einsetzen(elektroanlage);
-	Serial.println(elektroanlage.gib_log_zeile());
-	Serial.printf("Free stack: %u heap: %u\n", ESP.getFreeContStack(), ESP.getFreeHeap());
-	delay(5000);
+	web_presenter.webserver.watch_for_client();
+//	Local::WechselrichterLeser wechselrichter_leser(cfg, web_client);
+//	Local::ElektroAnlage elektroanlage;
+//	wechselrichter_leser.daten_holen_und_einsetzen(elektroanlage);
+//	Serial.println(elektroanlage.gib_log_zeile());
+//	Serial.printf("Free stack: %u heap: %u\n", ESP.getFreeContStack(), ESP.getFreeHeap());
+//	delay(5000);
 }
--- code/esp32-webserver/web_presenter.h
+++ code/esp32-webserver/web_presenter.h
@@ -3,10 +3,10 @@
 #include "wlan.h"
 #include "webserver.h"
 #include "web_client.h"
-#include "persistenz.h"
-#include "formatierer.h"
+//#include "persistenz.h"
+//#include "formatierer.h"
 #include "elektro_anlage.h"
-#include "wetter.h"
+//#include "wetter.h"
 //#include "smartmeter_leser.h"
 #include "wechselrichter_leser.h"
 //#include "wettervorhersage_leser.h"
@@ -18,12 +18,20 @@ namespace Local {
 		Local::Config cfg;
 
 		Local::ElektroAnlage elektroanlage;
-		Local::Wetter wetter;
-		Local::Persistenz persistenz;
+//		Local::Wetter wetter;
+//		Local::Persistenz persistenz;
 		Local::WebClient web_client;
 
-		void s(String content) {// TODO besser von SD laden!
-			webserver.server.sendContent(content);
+		char int_as_char[16];
+
+		void _print_char_to_web(char* c) {
+			webserver.server.sendContent(c);
+		}
+
+		void _print_int_to_web(int i) {
+			char int_as_char[16];
+			itoa(i, int_as_char, 10);
+			webserver.server.sendContent((char*) int_as_char);
 		}
 
 	public:
@@ -35,56 +43,68 @@ namespace Local {
 		}
 
 		void zeige_hauptseite() {
+			// Serial.println(printf("Date: %4d-%02d-%02d %02d:%02d:%02d\n", year(time), month(time), day(time), hour(time), minute(time), second(time)));
 			int now_timestamp = webserver.server.arg("time").toInt();
+			if(now_timestamp < 1674987010) {
+				webserver.server.send(400, "text/plain", "Bitte den aktuellen UnixTimestamp via Parameter 'time' angeben.");
+				return;
+			}
 
 			Local::WechselrichterLeser wechselrichter_leser(cfg, web_client);
 			wechselrichter_leser.daten_holen_und_einsetzen(elektroanlage);
 
-			Local::SmartmeterLeser smartmeter_leser(cfg, web_client);
-			smartmeter_leser.daten_holen_und_einsetzen(elektroanlage);
-
-			Local::WettervorhersageLeser wetter_leser(cfg, web_client);
-
-			if(now_timestamp > 1674987010) {// Gueltiger timestamp noetig
-				Serial.println("Time vorhanden");// TODO das muss beim Daten lesen immer da sein
-				// Serial.println(printf("Date: %4d-%02d-%02d %02d:%02d:%02d\n", year(time), month(time), day(time), hour(time), minute(time), second(time)));
-				String last_weather_request_timestamp = persistenz.read_file_content((char*) "last_weather_request.txt");
-				if(
-					(
-						last_weather_request_timestamp.toInt() < now_timestamp - 60*45// max alle 45min
-						&& minute(now_timestamp) < 15
-						&& minute(now_timestamp) >= 3// immer kurz nach um, damit die ForecastAPI Zeit hat
-					)
-					|| webserver.server.arg("reset").toInt() == 1 // DEBUG
-				) {// Insgesamt also 1x die Stunde ca 3 nach um
-					Serial.println("Reset");
-					wetter_leser.daten_holen_und_persistieren(persistenz);
-					persistenz.write2file((char*) "last_weather_request.txt", (String) now_timestamp);
-				}
-
-				wetter_leser.persistierte_daten_einsetzen(persistenz, wetter);
-				persistenz.append2file(
-					(char*) "anlage.csv",
-					(String) now_timestamp + ";"
-					+ elektroanlage.gib_log_zeile() + ";"
-					+ wetter.gib_log_zeile()
-				);
-			}
+//			Local::SmartmeterLeser smartmeter_leser(cfg, web_client);
+//			smartmeter_leser.daten_holen_und_einsetzen(elektroanlage);
+//
+//			Local::WettervorhersageLeser wetter_leser(cfg, web_client);
+//
+//			String last_weather_request_timestamp = persistenz.read_file_content((char*) "last_weather_request.txt");
+//			if(
+//				(
+//					last_weather_request_timestamp.toInt() < now_timestamp - 60*45// max alle 45min
+//					&& minute(now_timestamp) < 15
+//					&& minute(now_timestamp) >= 3// immer kurz nach um, damit die ForecastAPI Zeit hat
+//				)
+//				|| webserver.server.arg("reset").toInt() == 1 // DEBUG
+//			) {// Insgesamt also 1x die Stunde ca 3 nach um
+//				Serial.println("Reset");
+//				wetter_leser.daten_holen_und_persistieren(persistenz);
+//				persistenz.write2file((char*) "last_weather_request.txt", (String) now_timestamp);
+//			}
+//
+//			wetter_leser.persistierte_daten_einsetzen(persistenz, wetter);
+//			persistenz.append2file(
+//				(char*) "anlage.csv",
+//				(String) now_timestamp + ";"
+//				+ elektroanlage.gib_log_zeile() + ";"
+//				+ wetter.gib_log_zeile()
+//			);
 
 			webserver.server.setContentLength(CONTENT_LENGTH_UNKNOWN);
 			webserver.server.send(200, "application/json", "");
-			s((String) "{");
-			s((String) "\"max_i_in_ma\":" + (String) elektroanlage.max_i_in_ma() + (String) ",");
-			s((String) "\"max_i_phase\":" + elektroanlage.max_i_phase() + (String) ",");
-			s((String) "\"ueberschuss_in_wh\":" + (String) elektroanlage.gib_ueberschuss_in_wh() + (String) ",");
-			s((String) "\"solarakku_ladestand_in_promille\":" + (String) elektroanlage.solarakku_ladestand_in_promille + (String) ",");
-			s((String) "\"wolkendichte\":[");
-				s((String) wetter.gib_stundenvorhersage_wolkendichte(0));
-			s((String) "],");
-			s((String) "\"solarstrahlung\":[");
-				s((String) wetter.gib_stundenvorhersage_solarstrahlung(0));
-			s((String) "]");
-			s((String) "}");
+			_print_char_to_web((char*) "{");
+				_print_char_to_web((char*)  "\"max_i_in_ma\":");
+					_print_int_to_web(elektroanlage.max_i_in_ma());
+					_print_char_to_web((char*) ",");
+				_print_char_to_web((char*) "\"max_i_phase\":");
+					_print_int_to_web(elektroanlage.max_i_phase());
+					_print_char_to_web((char*) ",");
+
+				_print_char_to_web((char*) "\"ueberschuss_in_wh\":");
+					_print_int_to_web(elektroanlage.gib_ueberschuss_in_wh());
+					_print_char_to_web((char*) ",");
+
+				_print_char_to_web((char*) "\"solarakku_ladestand_in_promille\":");
+					_print_int_to_web(elektroanlage.solarakku_ladestand_in_promille);
+					_print_char_to_web((char*) ",");
+
+				_print_char_to_web((char*) "\"wolkendichte\":[");
+	//				s((String) wetter.gib_stundenvorhersage_wolkendichte(0));
+				_print_char_to_web((char*) "],");
+				_print_char_to_web((char*) "\"solarstrahlung\":[");
+	//				s((String) wetter.gib_stundenvorhersage_solarstrahlung(0));
+				_print_char_to_web((char*) "]");
+			_print_char_to_web((char*) "}");
 		}
 	};
 }
