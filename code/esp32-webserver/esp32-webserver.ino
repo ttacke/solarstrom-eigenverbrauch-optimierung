3347624ff013066bcbb4667940ce5a284d54127b
ttacke <ttacke@seibert-media.net>
2023-02-02 19:16:01 +0100

    Obsoletes entfernt

--- code/esp32-webserver/esp32-webserver.ino
+++ code/esp32-webserver/esp32-webserver.ino
@@ -1,7 +1,6 @@
 // TODO bis auf ein Minimum ist alles deaktiviert, um das Speicherproblem zu beheben
 /* TODO Als naechstes:
 - HTML Datei von SD-Karte ausliefern
-- Daten als JSON ausgeben
 - Stromdaten auch in JSON einfuegen
 - Wetterdaten in JSON einfuegen
 - Wetterdaten in UI darstellen
@@ -10,8 +9,6 @@
 #include "wlan.h"
 #include "web_client.h"
 #include "web_presenter.h"
-#include "elektro_anlage.h"
-#include "wechselrichter_leser.h"
 
 Local::Config cfg;
 Local::Wlan wlan(cfg.wlan_ssid, cfg.wlan_pwd);
@@ -35,10 +32,5 @@ void setup(void) {
 
 void loop(void) {
 	web_presenter.webserver.watch_for_client();
-//	Local::WechselrichterLeser wechselrichter_leser(cfg, web_client);
-//	Local::ElektroAnlage elektroanlage;
-//	wechselrichter_leser.daten_holen_und_einsetzen(elektroanlage);
-//	Serial.println(elektroanlage.gib_log_zeile());
-//	Serial.printf("Free stack: %u heap: %u\n", ESP.getFreeContStack(), ESP.getFreeHeap());
-//	delay(5000);
+	delay(1000);// Ein bisschen ausgebremst ist er immer noch schnell genug
 }
