8f094f7ec9344e64c0348ba2c9923c4705843f54
ttacke <ttacke@seibert-media.net>
2023-02-02 10:16:35 +0100

    Obsoletes entfernt

--- code/esp32-webserver/web_client.h
+++ code/esp32-webserver/web_client.h
@@ -126,27 +126,5 @@ namespace Local {
 			content_length = 0;
 			return false;
 		}
-
-		String get(const char* url) {// TODO DEPRECATED
-			HTTPClient http;
-			http.begin(wlan_client, url);
-			http.addHeader("Content-Type", "application/json");
-			http.setTimeout(15000);// ms, vermutlich
-			int httpCode = http.GET();
-			if (httpCode > 0) {
-				if (httpCode == HTTP_CODE_OK) {
-				  const String payload = http.getString();
-				  http.end();
-				  return payload;
-			  } else {
-				Serial.printf("[HTTP] GET failed, code: %d\n", httpCode);
-			  }
-			} else {
-			  Serial.printf("[HTTP] GET failed, error: ");
-			  Serial.printf(http.errorToString(httpCode).c_str());
-			}
-			http.end();
-			return "";
-		}
 	};
 }
