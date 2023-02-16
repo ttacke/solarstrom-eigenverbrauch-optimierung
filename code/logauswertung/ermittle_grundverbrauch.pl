f4adaef49ee7fd295a47f5bb8dc123b93033c104
ttacke <ttacke@seibert-media.net>
2023-02-16 18:24:56 +0100

    Datenverarbeitung begonnen

--- code/logauswertung/ermittle_grundverbrauch.pl
+++ code/logauswertung/ermittle_grundverbrauch.pl
@@ -2,13 +2,38 @@
 use strict;
 use warnings;
 
-my $verbrauch = [];
-open(my $fh, '<', './anlagen.log') or die "Bitte erst 'lade_aktuelle_logdatei.pl' ausfuehren\n";
+my $daten = [];
+open(my $fh, '<', './anlagen.log') or die "Bitte erst 'lade_aktuelle_logdatei.pl [ESP-IP]' ausfuehren\n";
 $/ = "\n";
 while(my $line = <$fh>) {
-    next if($line !~ m/\d+,ev1,[\-\d\.]+,[\-\d\.]+,[\-\d\.]+,[\-\d\.]+,([\-\d\.]+)/);
+    chomp($line);
 
-    push(@$verbrauch, $1) if($1 > 0);
+    my @e = $line =~ m/^(\d+),e1,([\-\d]+),([\-\d]+),(\d+),(\d+),(\d+),([\-\d]+),([\-\d]+),([\-\d]+),(\d+),w1,(\d+),(\d+)$/;
+    next if(!scalar(@e));
+
+    push(@$daten, {
+        zeitpunkt => $e[0],
+        netzbezug_in_wh => $e[1],
+        solarakku_zuschuss_in_wh => $e[2],
+        solarerzeugung_in_wh => $e[3],
+        stromverbrauch_in_wh => $e[4],
+        solarakku_ladestand_in_promille => $e[5],
+        l1_strom_ma => $e[6],
+        l2_strom_ma => $e[7],
+        l3_strom_ma => $e[8],
+        gib_anteil_pv1_in_prozent => $e[9],
+        stunden_solarstrahlung => $e[10],
+        tages_solarstrahlung => $e[11],
+    });
 }
 close($fh);
-print scalar(@$verbrauch) . " Datensaetze, Grundverbrauch: " . (sort(@$verbrauch))[int(scalar(@$verbrauch) / 2)] . " W\n";
+
+print "\nDatensaetze: " . scalar(@$daten) . "\n";
+
+my $verbrauch = [];
+foreach my $e (@$daten) {
+    push(@$verbrauch, $e->{stromverbrauch_in_wh});
+}
+print "Grundverbrauch: " . (sort(@$verbrauch))[int(scalar(@$verbrauch) / 2)] . " W\n";
+
+print "\n";
--- code/logauswertung/lade_aktuelle_logdatei.pl
+++ code/logauswertung/lade_aktuelle_logdatei.pl
@@ -10,7 +10,7 @@ if(system("wget --version 1>/dev/null 2>&1") != 0) {
     print "Bitte das Tool 'wget' installieren\n";
     exit(1);
 }
-if(system("wget http://192.168.0.15/zeige_log -O anlagen.log") == 0) {
+if(system("wget http://$ARGV[0]/zeige_log -O anlagen.log") == 0) {
     print "Erfolg\n";
     exit(0);
 }
