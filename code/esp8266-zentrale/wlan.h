3e805ee93ee567eb623ef3acc2009af916700ea8
ttacke <ttacke@seibert-media.net>
2023-02-22 09:45:49 +0100

    WLAN Reconnect eingefügt

--- code/esp8266-zentrale/wlan.h
+++ code/esp8266-zentrale/wlan.h
@@ -8,10 +8,12 @@ namespace Local {
 		WiFiClient client;
 		const char* ssid;
 		const char* pwd;
+
 		Wlan(const char* ssid, const char* pwd): ssid(ssid), pwd(pwd) {
 		}
+
 		void connect() {
-			Serial.println("Try to conect to WLAN " + (String) ssid);
+			Serial.println("Try to connect to WLAN " + (String) ssid);
 			WiFi.mode(WIFI_STA);
 			WiFi.begin(ssid, pwd);
 			while (WiFi.status() != WL_CONNECTED) {
@@ -19,6 +21,8 @@ namespace Local {
 				Serial.print(".");
 			}
 			Serial.println("\nWLAN Connected, IP address: " + WiFi.localIP().toString() + "\n");
+			WiFi.setAutoReconnect(true);
+			WiFi.persistent(true);
 		}
 	};
 }
