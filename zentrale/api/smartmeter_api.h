5a06c21c5eac70be4641e75a3cf727f30efda23e
ttacke <ttacke@seibert-media.net>
2025-03-25 07:54:48 +0100

    offizielle API benutzt

--- zentrale/api/smartmeter_api.h
+++ zentrale/api/smartmeter_api.h
@@ -10,15 +10,15 @@ namespace Local::Api {
 		std::uint8_t findings;
 
 		void _daten_extrahieren_und_einsetzen(Local::Model::ElektroAnlage& elektroanlage) {
-			if(web_reader->find_in_buffer((char*) "\"SMARTMETER_CURRENT_01_F64\" *: *([-0-9.]+)[^0-9]")) {
+			if(web_reader->find_in_buffer((char*) "\"Current_AC_Phase_1\" *: *([-0-9.]+)[^0-9]")) {
 				elektroanlage.l1_strom_ma = round(atof(web_reader->finding_buffer) * 1000);
 				findings |= 0b0000'0001;
 			}
-			if(web_reader->find_in_buffer((char*) "\"SMARTMETER_CURRENT_02_F64\" *: *([-0-9.]+)[^0-9]")) {
+			if(web_reader->find_in_buffer((char*) "\"Current_AC_Phase_2\" *: *([-0-9.]+)[^0-9]")) {
 				elektroanlage.l2_strom_ma = round(atof(web_reader->finding_buffer) * 1000);
 				findings |= 0b0000'0010;
 			}
-			if(web_reader->find_in_buffer((char*) "\"SMARTMETER_CURRENT_03_F64\" *: *([-0-9.]+)[^0-9]")) {
+			if(web_reader->find_in_buffer((char*) "\"Current_AC_Phase_3\" *: *([-0-9.]+)[^0-9]")) {
 				elektroanlage.l3_strom_ma = round(atof(web_reader->finding_buffer) * 1000);
 				findings |= 0b0000'0100;
 			}
@@ -29,7 +29,7 @@ namespace Local::Api {
 			web_reader->send_http_get_request(
 				cfg->wechselrichter_host,
 				cfg->wechselrichter_port,
-				"/components/PowerMeter/readable"
+				"/solar_api/v1/GetMeterRealtimeData.cgi"
 			);
 			findings = 0b0000'0000;
 			while(web_reader->read_next_block_to_buffer()) {
