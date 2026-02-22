3aed1525b35fa9c56853385dbcbc2662eff4cf9a
ttacke <ttacke@seibert-media.net>
2026-02-22 14:02:08 +0100

    WebReader Test begonnen

--- netz-relay/test/test_web_reader.cpp
+++ netz-relay/test/test_web_reader.cpp
@@ -19,6 +19,28 @@ void test_find_in_buffer_findet_content() {
     printf("test_find_in_buffer_findet_content OK\n");
 }
 
+void test_find_in_buffer_findet_content_ueber_chunks() {
+    WiFiClient client;
+    client.set_response(
+        "HTTP/1.1 200 OK\r\n"
+        "Transfer-Encoding: chunked\r\n"
+        "\r\n"
+        "1\r\n"
+        "o"
+        "1\r\n"
+        "k"
+        "0\r\n"
+        "\r\n"
+    );
+    Local::WebReader reader(client);
+    reader.send_http_get_request("test", 80, "/");
+    reader.read_next_block_to_buffer();
+    reader.read_next_block_to_buffer();
+    assert(reader.find_in_buffer((char*)"ok"));
+    printf("test_find_in_buffer_findet_content_ueber_chunks OK\n");
+}
+
 int main() {
     test_find_in_buffer_findet_content();
+    test_find_in_buffer_findet_content_ueber_chunks();
 }
